4f384490301acb802ac14603b6de3b4d439f8263
ninlawan <ninlawan2212@gmail.com>
2025-07-25 04:07:15 +0700

    mark 4

--- 331-intro-to-vite/src/views/event/RegisterView.vue
+++ 331-intro-to-vite/src/views/event/RegisterView.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-import { toRefs} from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import { type Event } from '@/types'
 
-
 const props = defineProps<{
- event: Event
- id: String
+  event: Event
+  id: string
 }>()
-const {event} = toRefs(props)
+const router = useRouter()
+const register = () => {
+  router.push({ name: 'event-detail-view' })
+}
 </script>
 <template>
-    <p>Register here</p>
+  <p>Register here</p>
+  <button @click="register">Register</button>
 </template>
