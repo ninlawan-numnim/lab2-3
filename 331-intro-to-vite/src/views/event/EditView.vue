16fae302818a1344e245d33fd94005ba30fad6d2
ninlawan <ninlawan2212@gmail.com>
2025-08-01 06:15:59 +0700

    lab4 1

--- 331-intro-to-vite/src/views/event/EditView.vue
+++ 331-intro-to-vite/src/views/event/EditView.vue
@@ -1,14 +1,50 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
   event: Event
   id: String
 }>()
 const { event } = toRefs(props)
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const router = useRouter()
+const store = useMessageStore()
+
+const updateEvent = () => {
+  store.updateMessage('The data has been updated!')
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({ 
+    name: 'event-detail-view', 
+    params: { id: props.event.id },
+    // query: { message: 'The data has been updated!' }
+  })
+}
 </script>
+
 <template>
-  <p>Edit event here</p>
+  <div>    
+    <button @click="updateEvent" class="update-btn">
+      Update Event
+    </button>
+  </div>
 </template>
+
+<style scoped>
+.update-btn {
+  background-color: #42b983;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+.update-btn:hover {
+  background-color: #369870;
+}
+</style>
--- 331-intro-to-vite/src/views/event/RegisterView.vue
+++ 331-intro-to-vite/src/views/event/RegisterView.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { useRouter, useRoute } from 'vue-router'
 import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
   event: Event
   id: string
 }>()
 const router = useRouter()
+const store = useMessageStore()
+
 const register = () => {
-  router.push({ name: 'event-detail-view' })
+  store.updateMessage('You are successfully registered for ' + props.event.title)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({
+    name: 'event-detail-view',
+    params: { id: props.event.id }
+  })
 }
 </script>
 <template>
